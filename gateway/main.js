c0ff8bba91b0d280c1ee6c767d21d25083550461
André Rosa <25556799+andreffrosa@users.noreply.github.com>
2022-03-18 16:18:31 +0000

    v1.0.5

--- gateway/main.js
+++ gateway/main.js
@@ -2,20 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const clock = require('monotonic-timestamp');
 const jwt = require('jsonwebtoken');
-const { signatureVerify } = require('@polkadot/util-crypto');
-const { hexToU8a } = require('@polkadot/util');
+const {
+    signatureVerify
+} = require('@polkadot/util-crypto');
+const {
+    hexToU8a
+} = require('@polkadot/util');
 const pg = require('pg');
 const fs = require('fs/promises');
 const net = require('net-promise');
-const readline = require('readline');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+//const readline = require('readline');
+const csv = require('csv-parser');
+const {
+    createProxyMiddleware
+} = require('http-proxy-middleware');
+const cors = require('cors');
 const log4js = require("log4js");
 
-function Env(key, default_value=undefined) {
-    if(process.env[key]) {
+// Auxiliary Utility Functions
+const Env = (key, default_value = undefined) => {
+    if (process.env[key]) {
         return process.env[key];
     } else {
-        if(default_value) {
+        if (default_value) {
             return default_value;
         } else {
             throw `Invalid environment variable ${key}`;
@@ -25,6 +34,12 @@ function Env(key, default_value=undefined) {
 
 const Sleep = ms => new Promise(r => setTimeout(r, ms));
 
+const Reply = (status = 200, body = "") => ({
+    status: status,
+    body: body
+});
+
+// Server Configurations
 const IMG_TAG = Env("IMG_TAG");
 
 const SERVER_PORT = Number(Env("SERVER_PORT", 80));
@@ -45,54 +60,72 @@ const POSTGRES_PORT = Number(Env("POSTGRES_PORT"));
 
 // Configure Logger
 log4js.configure({
-  appenders: { out: { type: 'stdout', layout: {
-      type: 'pattern',
-      pattern: '%[[%d{dd-MM-yyyy hh:mm:ss}] %p ::%] %m',
-  }}},
-  categories: { default: { appenders: [ 'out' ], level: 'debug' } }
+    appenders: {
+        out: {
+            type: 'stdout',
+            layout: {
+                type: 'pattern',
+                pattern: '%[[%d{dd-MM-yyyy hh:mm:ss}] %p ::%] %m',
+            }
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['out'],
+            level: 'debug'
+        }
+    }
 });
 var log = log4js.getLogger();
 
 // Configure Express
-const router = express.Router();
+const login_resource = express.Router();
 const server = express();
-server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.urlencoded({
+    extended: false
+}));
 server.use(bodyParser.json());
+server.use(cors({
+    origin: '*'
+}));
 
-// Configure DB Connection
+// Configure DB
 const db = new pg.Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  port: POSTGRES_PORT,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    port: POSTGRES_PORT,
 });
 
 // Auxiliary Functions and Variables
 
 async function checkConnection(dep) {
-    return new Promise( async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         var aux = dep.split(":");
         var host = aux[0];
         var port = Number(aux[1] ? aux[1] : "80");
 
         var connected = false;
-        for(var i = 0; i < N_CONNECTION_TRIES && !connected; i++) {
+        for (var i = 0; i < N_CONNECTION_TRIES && !connected; i++) {
             try {
 
-                var client = await net.Socket({host: host, port: port});
+                var client = await net.Socket({
+                    host: host,
+                    port: port
+                });
                 connected = true;
                 client.close();
 
-            } catch(err) {
+            } catch (err) {
                 // Ignore and try again
                 await Sleep(1000);
 
                 log.debug(`Re-trying to connect to ${dep} (attempt ${i+1}/${N_CONNECTION_TRIES}) ...`);
             }
         }
-        if(N_CONNECTION_TRIES > 0) {
-            if(!connected) {
+        if (N_CONNECTION_TRIES > 0) {
+            if (!connected) {
                 reject(Error(`Cannot connect to ${dep}`));
             } else {
                 log.debug(`Connected to ${dep}`);
@@ -102,10 +135,10 @@ async function checkConnection(dep) {
         } else {
             resolve();
         }
-    } );
+    });
 }
 
-async function dependencies(deps) {
+async function onReady(deps) {
     var px = [];
 
     deps.forEach((dep, i) => {
@@ -119,34 +152,43 @@ async function dependencies(deps) {
 async function fillDB() {
 
     // Create Table
-    var res = await db.query('CREATE TABLE whitelist (address varchar(255), last_timestamp varchar(255));');
+    var res = await db.query('CREATE TABLE whitelist (address VARCHAR(255) PRIMARY KEY, name VARCHAR(255), enabled BOOLEAN, last_timestamp BIGINT);');
 
     // Insert Entries
     try {
         await fs.access(WHITELIST_FILE, require('fs').constants.R_OK);
 
-        var p = new Promise( async (resolve, reject) => {
+        var fd = await fs.open(WHITELIST_FILE);
+
+        return new Promise(async (resolve, reject) => {
             var px = [];
             var inserted = 0;
 
-            var fd = await fs.open(WHITELIST_FILE);
-            var reader = readline.createInterface({
-              input: fd.createReadStream(),
-              crlfDelay: Infinity
-            });
+            const parseRow = async (raw_row) => {
+                console.log(raw_row);
 
-            for await (const line of reader) {
-                var p = db.query("INSERT INTO whitelist (address, last_timestamp) VALUES ($1, $2);", [line.trim(), 0]);
+                var address = raw_row.address?.trim();
+                var name = String(raw_row.name?.trim());
+                var enabled = raw_row.enabled?.trim() == "true";
+                var last_timestamp = 0;
 
-                inserted++;
+                var p = db.query("INSERT INTO whitelist (address, name, enabled, last_timestamp) VALUES ($1, $2, $3, $4);", [address, name, enabled, last_timestamp]);
 
                 px.push(p);
             }
-            Promise.all(px);
-            resolve(inserted);
-        });
 
-        return await p;
+            fd.createReadStream()
+                .pipe(csv({
+                    separator: ';'
+                }))
+                .on('data', parseRow)
+                .on('end', async () => {
+                    var inserted = (await Promise.all(px)).length;
+                    resolve(inserted);
+
+                    // console.log("WHITELIST", (await db.query('SELECT * FROM whitelist')).rows);
+                });
+        });
     } catch (err) {
         log.debug(err);
 
@@ -158,8 +200,8 @@ async function initDB() {
 
     var table_exists = (await db.query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name='whitelist');")).rows[0]?.exists;
 
-    if( table_exists ) {
-        if( RESET_WHITELIST ) {
+    if (table_exists) {
+        if (RESET_WHITELIST) {
             await db.query('DROP TABLE whitelist;');
 
             log.debug("Whitelist cleared!");
@@ -167,7 +209,7 @@ async function initDB() {
             var n_inserted = await fillDB();
             log.debug(`Inserted ${n_inserted} entries in whitelist.`);
         } else {
-            var n_addresses = (await db.query("SELECT COUNT() FROM whitelist;")).rows[0]?.count;
+            var n_addresses = (await db.query("SELECT COUNT(address) FROM whitelist;")).rows[0]?.count;
 
             log.debug(`Whitelist already exists with ${n_addresses} addresses.`);
         }
@@ -178,118 +220,149 @@ async function initDB() {
 }
 
 async function getLastTimestamp(address) {
-    return (await db.query('SELECT last_timestamp FROM whitelist WHERE address=$1', [address])).rows[0]?.last_timestamp;
+    return (await db.query("SELECT last_timestamp FROM whitelist WHERE address=$1 AND enabled='true'", [address])).rows[0]?.last_timestamp;
 }
 
 async function setLastTimestamp(address, ts) {
     await db.query('UPDATE whitelist SET last_timestamp = $1 WHERE address = $2', [ts, address]);
 }
 
-function validSignature(address, signature, data) {
-    return signatureVerify(data, hexToU8a(signature), address).isValid;
+function verifySignature(address, signature, data) {
+    try {
+        return signatureVerify(data, hexToU8a(signature), address).isValid;
+    } catch (err) {
+        return false;
+    }
 }
 
 function getAuthToken(address) {
-    return jwt.sign({address: address}, JWT_SECRET, { expiresIn: JWT_DURATION+'s' });
+    return jwt.sign({
+        address: address
+    }, JWT_SECRET, {
+        expiresIn: `${JWT_DURATION}s`
+    });
+}
+
+function verifyAuthToken(token) {
+    if (!token || token == "") {
+        return Reply(401, 'No Token Provided');
+    }
+
+    try {
+        var decoded = jwt.verify(token, JWT_SECRET);
+
+        // console.log(Date.now(), decoded);
+
+        return Reply(200, decoded);
+    } catch (err) {
+        return Reply(403, err.message);
+    }
+}
+
+async function login(address, signature, timestamp) {
+    try {
+        if (!address) {
+            return Reply(400, "Invalid Login Request : No address");
+        }
+
+        if (!signature) {
+            return Reply(400, "Invalid Login Request : No signature");
+        }
+
+        if (!timestamp) {
+            return Reply(400, "Invalid Login Request : No timestamp");
+        }
+        var n_timestamp = Number(timestamp);
+
+        // Verify if received timestamp is valid
+        if (isNaN(n_timestamp)) {
+            return Reply(401, "Invalid Login Request : Timestamp is NaN");
+        }
+
+        // Verify if received timestamp is older than current time
+        if (n_timestamp >= clock()) {
+            return Reply(401, "Invalid Timestamp : Future Timestamp");
+        }
+
+        // Verify if the address is whitelisted
+        var last_timestamp = await getLastTimestamp(address);
+        if (!last_timestamp) {
+            return Reply(403, "Not Whitelisted");
+        }
+
+        // Verify if timestamp is fresh
+        if (last_timestamp >= n_timestamp) {
+            return Reply(401, "Replayed Timestamp");
+        }
+
+        // Verify if it is a valid signature
+        var valid_signature = await verifySignature(address, signature, timestamp);
+        if (!valid_signature) {
+            return Reply(401, "Invalid Signature");
+        }
+
+        // Login suceded, update last_timestamp and return a new jwt
+
+        await setLastTimestamp(address, n_timestamp);
+
+        var token = getAuthToken(address);
+
+        return Reply(200, token); // { auth: true, token: token }
+    } catch (err) {
+        return Reply(500, err.message);
+    }
 }
 
 // API Endpoints
 
-router.get('/', (req,res) => {
+login_resource.get('/', (req, res) => {
     // Retrieve and send current monotonic timestamp
-    res.send(clock().toString()).end();
+    res.json(clock().toString());
 });
 
-router.post('/', async (req,res) => {
+login_resource.post('/', async (req, res) => {
+    // Parse parameters
+    var address = req.body?.address;
+    var signature = req.body?.signature;
+    var timestamp = req.body?.timestamp;
 
-    var address = undefined;
-    var signature = undefined;
-    var timestamp = undefined;
-    var msg = "";
+    // Login
+    var reply = await login(address, signature, timestamp);
 
-    // Verify if the request is valid
-    if(req.body.address && req.body.signature && req.body.timestamp) {
-        address = req.body.address;
-        signature = req.body.signature;
-        timestamp = req.body.timestamp;
+    // Send Response
+    res.status(reply.status).json(reply.body);
 
-        // Verify if received timestamp is valid
-        var n_timestamp = Number(timestamp);
-        if(n_timestamp < clock()) {
-
-            // Verify if the address is whitelisted
-            var last_timestamp = await getLastTimestamp(address);
-            var is_whitelisted = last_timestamp != null && last_timestamp != undefined;
-            if(is_whitelisted) {
-
-                // Verify if timestamp is fresh
-                if(last_timestamp < n_timestamp) {
-                    // Verify if it is a valid signature
-                    if(validSignature(address, signature, timestamp)) {
-
-                        await setLastTimestamp(address, n_timestamp);
-
-                        var token = getAuthToken(address);
-
-                        msg = "";
-                        res.json(token);
-                        // return res.json({ auth: true, token: token });
-                    } else {
-                        msg = "Invalid Signature";
-                        res.status(401).send(msg);
-                    }
-                } else {
-                    msg = "Replayed Timestamp";
-                    res.status(401).send(msg);
-                }
-            } else {
-                msg = "Not Whitelisted";
-                res.status(403).send(msg);
-            }
-        } else {
-            msg = "Invalid Timestamp";
-            res.status(401).send(msg);
-        }
-    } else {
-        msg = "Invalid Login Request";
-        res.status(400).send(msg);
-    }
-
-    // Log request
-    var str = (msg!=""? " : " + msg : "");
-    log.debug(`LOGIN: ${address} -> ${res.statusCode} ${res.statusMessage}${str}`);
-    res.end();
+    // Log
+    var str = (reply.status == 200 ? "" : " : " + reply.body);
+    log.debug(`LOGIN ${address} -> ${res.statusCode} ${res.statusMessage}${str}`);
 });
 
-// Add router to the Express server
-server.use("/login", router);
+// Register the login resource
+server.use("/login", login_resource);
 
-// Authorization
+// Enforce authorization verification of every non-login request
 server.use('', (req, res, next) => {
+    // Retrieve token from request headers
     const x = req.headers['x-access-token'];
     const a = req.headers['authorization'];
     const token = x ? x : (a ? a.split(' ')[1] : null);
 
-    if(token) {
-        try {
-            var decoded = jwt.verify(token, JWT_SECRET);
-            //console.log(decoded);
+    // Verify token
+    var reply = verifyAuthToken(token);
 
-            next();
-        } catch(err) {
-            //console.log(err);
-            res.status(403).send(err.message);
-        }
+    if (reply.status == 200) {
+        next();
     } else {
-        //res.status(401).json({ auth: false, message: 'No token provided.' });
-        res.status(401).send('No token provided.');
+        res.status(reply.status).send(reply.body);
     }
 });
 
 // Proxy endpoints
 server.use('/', createProxyMiddleware({
     target: BACKEND_URL,
-    changeOrigin: false
+    changeOrigin: false,
+    ws: true,
+    //logLevel: 'debug',
     /*pathRewrite: {
         [`^/json_placeholder`]: '',
     },*/
@@ -300,17 +373,19 @@ async function main() {
     log.debug(`crypto-login ${IMG_TAG}`);
 
     // Wait for the dependencies to initialize
-    await dependencies([
+    const dependencies = [
         `${POSTGRES_HOST}:${POSTGRES_PORT}`,
         BACKEND_URL.replace(/http[s]?:\/\//, "")
-    ]);
+    ];
+    await onReady(dependencies);
 
     // Initialize the database
     await initDB();
 
     // Start the web server
-    /*const sv = */await server.listen(SERVER_PORT);
+    /*const sv = */
+    await server.listen(SERVER_PORT);
 
-    log.debug(`Listening on tcp/${SERVER_PORT} ...\n`);
+    log.debug(`Listening on TCP/${SERVER_PORT} ...\n`);
 }
 main();
--- test/test.js
+++ test/test.js
@@ -1,20 +1,26 @@
 const {
-  mnemonicGenerate,
-  mnemonicToMiniSecret,
-  mnemonicValidate,
-  naclKeypairFromSeed
+    mnemonicGenerate,
+    mnemonicToMiniSecret,
+    mnemonicValidate,
+    naclKeypairFromSeed
 } = require('@polkadot/util-crypto');
-const { stringToU8a, u8aToHex } = require('@polkadot/util');
-const { Keyring } = require('@polkadot/keyring');
+const {
+    stringToU8a,
+    u8aToHex
+} = require('@polkadot/util');
+const {
+    Keyring
+} = require('@polkadot/keyring');
 const axios = require('axios');
 const net = require('net-promise');
 //const pg = require('pg');
 
-function Env(key, default_value=undefined) {
-    if(process.env[key]) {
+// Auxiliary Utility Functions
+const Env = (key, default_value = undefined) => {
+    if (process.env[key]) {
         return process.env[key];
     } else {
-        if(default_value) {
+        if (default_value) {
             return default_value;
         } else {
             throw `Invalid environment variable ${key}`;
@@ -24,45 +30,65 @@ function Env(key, default_value=undefined) {
 
 const Sleep = ms => new Promise(r => setTimeout(r, ms));
 
-//const LOGIN_URL = Env("LOGIN_URL");
+const Assert = (expr, msg = undefined) => {
+    var status, color;
+    if (expr) {
+        status = "OK";
+        color = "32";
+    } else {
+        status = "FAILED";
+        color = "31";
+    }
+
+    var str = "";
+    if (msg) {
+        str = ` : ${String(msg).trim()}`;
+    }
+
+    console.log(`\x1b[${color}m [${status}] \x1b[0m ${str}`);
+};
+
 const API_URL = Env("API_URL");
 const N_CONNECTION_TRIES = Number(Env("N_CONNECTION_TRIES", 10));
 
 
 async function genAddress() {
-  // Create mnemonic string for Alice using BIP39
-  const mnemonic = mnemonicGenerate();
+    // Create mnemonic string for Alice using BIP39
+    const mnemonic = mnemonicGenerate();
 
-  console.log(`Mnemonic: ${mnemonic}`);
+    console.log(`Mnemonic: ${mnemonic}`);
 
-  // Validate the mnemic string that was generated
-  const isValidMnemonic = mnemonicValidate(mnemonic);
-  // console.log(`isValidMnemonic: ${isValidMnemonic}`);
+    // Validate the mnemic string that was generated
+    const isValidMnemonic = mnemonicValidate(mnemonic);
+    // console.log(`isValidMnemonic: ${isValidMnemonic}`);
 
-  const keyring = new Keyring();
+    const keyring = new Keyring();
 
-  // Create and add the pair to the keyring
-  const pair = keyring.addFromUri(mnemonic);
+    // Create and add the pair to the keyring
+    const pair = keyring.addFromUri(mnemonic);
 
-  // Print the address (encoded with the ss58Format)
-  console.log('Address: ', pair.address);
+    // Print the address (encoded with the ss58Format)
+    console.log('Address: ', pair.address);
 }
 
 async function checkConnection(dep) {
-    return new Promise( async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         var aux = dep.split(":");
         var host = aux[0];
         var port = Number(aux[1] ? aux[1] : "80");
 
         var connected = false;
-        for(var i = 0; i < N_CONNECTION_TRIES && !connected; i++) {
+        for (var i = 0; i < N_CONNECTION_TRIES && !connected; i++) {
             try {
 
-                var client = await net.Socket({host: host, port: port});
+                var client = await net.Socket({
+                    host: host,
+                    port: port
+                });
                 connected = true;
                 client.close();
 
-            } catch(err) {
+            } catch (err) {
                 //console.log(err);
                 // Ignore and try again
                 await Sleep(1000);
@@ -70,8 +96,8 @@ async function checkConnection(dep) {
                 console.log(`Re-trying to connect to ${dep} (attempt ${i+1}/${N_CONNECTION_TRIES}) ...`);
             }
         }
-        if(N_CONNECTION_TRIES > 0) {
-            if(!connected) {
+        if (N_CONNECTION_TRIES > 0) {
+            if (!connected) {
                 reject(Error(`Cannot connect to ${dep}`));
             } else {
                 console.log(`Connected to ${dep}`);
@@ -81,7 +107,7 @@ async function checkConnection(dep) {
         } else {
             resolve();
         }
-    } );
+    });
 }
 
 async function dependencies(deps) {
@@ -95,54 +121,125 @@ async function dependencies(deps) {
     return Promise.all(px);
 }
 
-async function login(keypair) {
-    var resp = await axios.get(API_URL + "/login");
-    var timestamp = resp.data;
+async function login(keypair, sig = undefined, ts = undefined) {
+    try {
+        var resp = await axios.get(API_URL + "/login");
+        var timestamp = resp.data;
 
-    const signature = u8aToHex(keypair.sign(stringToU8a(timestamp)));
+        const signature = u8aToHex(keypair.sign(stringToU8a(timestamp)));
 
-    var credentials = {
-        address: keypair.address,
-        signature: signature,
-        timestamp: timestamp
-    };
+        var credentials = {
+            address: keypair.address,
+            signature: sig ? sig : signature,
+            timestamp: ts ? ts : timestamp
+        };
 
-    resp = await axios.post(API_URL + "/login", credentials);
-    return resp.data;
+        resp = await axios.post(API_URL + "/login", credentials);
+
+        return {
+            status: resp.status,
+            body: resp.data
+        };
+    } catch (err) {
+        return {
+            status: err.response?.status,
+            body: err.response?.data
+        };
+    }
 }
 
-async function get(path, jwt) {
+async function get(path, jwt, option = 1) {
     try {
         const config = {
-            headers: {
-                "x-access-token": jwt,
-                //authorization: `Bearer ${jwt}`
-            }
+            headers: {}
         };
+        if (jwt) {
+            if (option == 1) {
+                config.headers["x-access-token"] = jwt;
+            } else if (option == 2) {
+                config.headers.authorization = `Bearer ${jwt}`;
+            }
+        }
+
         var resp = await axios.get(API_URL + path, config);
-        return resp.data;
-    } catch(err) {
-        return {status: err.response?.status, message: err.response?.data};
+
+        return {
+            status: resp.status,
+            body: resp.data
+        };
+    } catch (err) {
+        return {
+            status: err.response?.status,
+            body: err.response?.data
+        };
     }
 }
 
 async function main() {
+    // Wait for web server
     await dependencies([API_URL.replace(/http[s]?:\/\//, "")]);
 
     // Create a keyring
     const keyring = new Keyring();
-    const mnemonic = Env("TEST_MNEMONIC");
-    const alice = keyring.addFromUri(mnemonic);
+    const alice = keyring.addFromUri(Env("TEST_MNEMONIC"));
+    const bob = keyring.addFromUri(mnemonicGenerate());
 
-    var jwt = await login(alice);
-    console.log("  > Alice" , alice.address, jwt != null);
+    var reply = null;
 
-    var res = await get("/", jwt);
-    console.log(res);
+    const Ax = (expected_status, reply) => Assert(reply.status == expected_status, `${reply.status} ${reply.body}`);
+
+    // Sucess Login
+    reply = await login(alice);
+    Ax(200, reply);
+    var token = reply.body;
+
+    // Not Whitelisted
+    reply = await login(bob);
+    Ax(403, reply);
+
+    // Invalid Signature
+    reply = await login(alice, "adarawdasd");
+    Ax(401, reply);
+
+    // Timestamp is NaN
+    reply = await login(alice, undefined, "hjvvilvhvl");
+    Ax(401, reply);
+
+    // Replayed Timestamp
+    reply = await login(alice, undefined, "1");
+    Ax(401, reply);
+
+    // Future Timestamp
+    reply = await login(alice, undefined, "99999999999999");
+    Ax(401, reply);
+
+    // Sucessfull Request
+    reply = await get("/", token);
+    Ax(200, reply);
+
+    // jwt malformed
+    reply = await get("/", "ausidfgas");
+    Ax(403, reply);
+
+    // No Token Provided
+    reply = await get("/", undefined);
+    Ax(401, reply);
+
+    // No Token Provided
+    reply = await get("/", null);
+    Ax(401, reply);
+
+    // No Token Provided
+    reply = await get("/", "");
+    Ax(401, reply);
+
+    // jwt expired
+    await Sleep(3100);
+    reply = await get("/", token);
+    Ax(403, reply);
 }
 
-
-if(process.argv[2] == "genaddress") {
+if (process.argv[2] == "genaddress") {
     genAddress().catch(console.error).finally(() => process.exit());
 } else {
     main().catch(console.error).finally(() => process.exit());
